b2870f4267c07b30f961cd1d2d3ef86a8e54a864
Aileen <538aileen@gmail.com>
2024-05-10 12:18:16 +0200

    terminado

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
 
-// mongoose.connect(process.env.MONGO_URL);
-// console.log('conexion a MongoDB Atlas')
+mongoose.connect(process.env.MONGO_URL);
+console.log('conexion a MongoDB Atlas')
 
-mongoose.connect('mongodb+srv://538aileen:AileenMontes1@cluster0.vpzjrqb.mongodb.net/', {
+// mongoose.connect('mongodb+srv://538aileen:AileenMontes1@cluster0.vpzjrqb.mongodb.net/', {
 
-}).then(() => {
-  console.log("Conexión a MongoDB Atlas establecida correctamente");
-}).catch(err => {
-  console.error("Error al conectar a MongoDB Atlas:", err);
-});
+// }).then(() => {
+//   console.log("Conexión a MongoDB Atlas establecida correctamente");
+// }).catch(err => {
+//   console.error("Error al conectar a MongoDB Atlas:", err);
+// });
+
 
